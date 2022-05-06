6e4871f17ad3433b5ffc0cda07e380eddd48c267
Nauta_64 <guillem_estevecosta@iescarlesvallbona.cat>
2022-05-06 19:23:16 +0200

    asdf

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import random
 
-from erfa import apio
+# from erfa import apio
 from flask import Flask
+from types import SimpleNamespace
 import requests
 import json
-from wordnik import *
-from wordnik import swagger, WordApi, WordsApi
-from wordnik.models.Related import Related
+
 
 from objectSyn import APIObject
 
-apiUrl = 'http://api.wordnik.com/v4'
-apiKey = 'ggr25tbylicxslhdhqyqg0wwimg6e0lbk5piwzc5oqrh62aa5'
-client = swagger.ApiClient(apiKey, apiUrl)
-from wordnik.models import WordObject, Definition
+# apiUrl = 'http://api.wordnik.com/v4'
+# apiKey = 'ggr25tbylicxslhdhqyqg0wwimg6e0lbk5piwzc5oqrh62aa5'
+# client = swagger.ApiClient(apiKey, apiUrl)
+# from wordnik.models import WordObject, Definition
 
 app = Flask(__name__, template_folder='templates')
 app.config['UPLOAD_FOLDER'] = r'files/'
@@ -22,6 +21,7 @@ app.config['UPLOAD_FOLDER'] = r'files/'
 
 @app.route('/<name>')
 def index(name):
+
     return '<h1>Hola {}!<h1>'.format(name)
 
 
@@ -31,70 +31,83 @@ def word5():
 
     file = open('words5.txt')
     content = file.readlines()
-    n = random.randint(0, 5749)
+    n = random.randint(0, 2309)
     json_string = json.dumps({"word": content[n].replace("\n","")})
     print(json_string)
     return json.loads(json_string)
 
 @app.route('/getWord')
 def word():
-    apiUrl = 'http://api.wordnik.com/v4'
-    apiKey = 'ggr25tbylicxslhdhqyqg0wwimg6e0lbk5piwzc5oqrh62aa5'
-
-    client = swagger.ApiClient(apiKey, apiUrl)
-    wordApi = WordsApi.WordsApi(client)
-    example: WordObject = wordApi.getRandomWord(minLength=5, maxLength=5)
-    json_string = json.dumps(example.__dict__)
-    return example.word
+    file = open('WordKeys.txt')
+    content = file.readlines()
+    n = random.randint(0, 32428)
+    return content[n].replace("\n", "")
 @app.route('/getDef')
 def get_definition():
-    apiUrl = 'http://api.wordnik.com/v4'
-    apiKey = 'ggr25tbylicxslhdhqyqg0wwimg6e0lbk5piwzc5oqrh62aa5'
-    client = swagger.ApiClient(apiKey, apiUrl)
-    wordApi = WordsApi.WordsApi(client)
-    example: WordObject = wordApi.getRandomWord()
 
-    wordApi = WordApi.WordApi(client)
-    example: WordObject = wordApi.getDefinitions(word=example.word,
-                                     sourceDictionaries='wiktionary')
-    text = example[len(example)-1].text.replace("<xref>","").replace("</xref>","")
-
-    json_string = json.dumps({"word":example[0].word,
-                              "text": text})
+    with open("wordsapi_sample.json") as jsonFile:
+        data = json.load(jsonFile)
+        file = open('WordKeys.txt')
+        content = file.readlines()
+        n = random.randint(0, 32428)
+        jsonData = data["dictionary"][content[n].replace('\n', '')]
+        print(jsonData)
+        while 'definitions' not in jsonData:
+            n = random.randint(0, 32428)
+            jsonData = data["dictionary"][content[n].replace('\n', '')]
+            print(jsonData)
+        json_string = json.dumps({"word": content[n].replace('\n', ''),
+                                "def": jsonData.get('definitions')[0].get('definition')})
     return json.loads(json_string)
 
 @app.route('/getSynonym')
 def get_synonym():
-    apiUrl = 'http://api.wordnik.com/v4'
-    apiKey = 'ggr25tbylicxslhdhqyqg0wwimg6e0lbk5piwzc5oqrh62aa5'
-    apiKey2 = 'c551e39a-4ef6-44b0-8070-51ebf8dac233'
-    client = swagger.ApiClient(apiKey, apiUrl)
-    wordApi = WordsApi.WordsApi(client)
-    examplew: WordObject = wordApi.getRandomWord(hasDictionaryDef=True)
-
-    wordApi = WordApi.WordApi(client)
-    synon: list[Related] = wordApi.getRelatedWords(examplew.word, relationshipTypes="synonym", useCanonical=True)
-
-    json_string = json.dumps({"word": examplew.word, "syn": synon[0].words})
+    with open("wordsapi_sample.json") as jsonFile:
+        data = json.load(jsonFile)
+        file = open('WordKeys.txt')
+        content = file.readlines()
+        hasDefinitions = False
+        hasSynonyms = False
+        while not hasDefinitions and not hasSynonyms:
+            n = random.randint(0, 32428)
+            jsonData = data["dictionary"][content[n].replace('\n', '')]
+            print(jsonData)
+            if 'definitions' in jsonData:
+                hasDefinitions = True
+            else:
+                hasDefinitions = False
+            while not hasSynonyms and hasDefinitions:
+                if isinstance(jsonData.get('definitions')[0].get('synonyms'), type(None)):
+                    print(jsonData)
+                    hasDefinitions = False
+                else:
+                    hasSynonyms = True
+                    break
+            if hasSynonyms:
+                break
+        print(jsonData)
+        json_string = json.dumps({"word": content[n].replace('\n', ''),
+                                  "syn": jsonData.get('definitions')[0].get('synonyms')})
 
     return json.loads(json_string)
 
 
 @app.route('/getAntonym')
 def get_antonym():
-    apiUrl = 'http://api.wordnik.com/v4'
-    apiKey = 'ggr25tbylicxslhdhqyqg0wwimg6e0lbk5piwzc5oqrh62aa5'
-    apiKey2 = 'c551e39a-4ef6-44b0-8070-51ebf8dac233'
-    client = swagger.ApiClient(apiKey, apiUrl)
-    wordApi = WordsApi.WordsApi(client)
-    examplew: WordObject = wordApi.getRandomWord(hasDictionaryDef=True)
+    # apiUrl = 'http://api.wordnik.com/v4'
+    # apiKey = 'ggr25tbylicxslhdhqyqg0wwimg6e0lbk5piwzc5oqrh62aa5'
+    # apiKey2 = 'c551e39a-4ef6-44b0-8070-51ebf8dac233'
+    # client = swagger.ApiClient(apiKey, apiUrl)
+    x = requests.get('https://random-word-api.herokuapp.com/word?')
 
-    wordApi = WordApi.WordApi(client)
-    synon: list[Related] = wordApi.getRelatedWords(examplew.word, relationshipTypes="antonym",useCanonical=True)
+    print(x.text)
 
-    json_string = json.dumps({"word": examplew.word, "syn": synon[0].words})
+    # wordApi = WordApi.WordApi(client)
+    # synon: list[Related] = wordApi.getRelatedWords(examplew.word, relationshipTypes="antonym",useCanonical=True)
+    #
+    # json_string = json.dumps({"word": examplew.word, "syn": synon[0].words})
 
-    return json.loads(json_string)
+    return json.loads(x.text)
 @app.route('/find=<word>')
 def searchW(word):
 
